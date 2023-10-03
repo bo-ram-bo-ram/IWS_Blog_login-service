b8bb8d9a05e0f43cd361fb72f5343f8b3bd2fa88
boram <qhkfa8253@naver.com>
2023-10-04 01:03:30 +0900

    initial commit

---
+++ src/main/java/ITS/IWS_blog/IwsBlogApplication.java
@@ -0,0 +1,13 @@
+package ITS.IWS_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwsBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IwsBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ITS/IWS_blog/IwsBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package ITS.IWS_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IwsBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
